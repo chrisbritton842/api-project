48276d4486f68393a8915fca2d8b9d106c4b5758
Chris Britton <chrisbritton790@gmail.com>
2022-01-12 13:04:17 -0800

    tweets handler

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../db/models');
+const { Tweet } = db;
 
-router.get("/tweets", (req, res) => {
-    res.json({message: "test tweets index"});
-  });
+const asyncHandler = handler => (req, res, next) => handler(req, res, next).catch(next);
 
-module.exports = router
+const tweetNotFoundError = (tweetId) => {
+  const error = new Error(`Tweet with id ${tweetId} not found.`);
+  error.title = 'Tweet not found.';
+  error.status = 404;
+  return error;
+}
+
+router.get("/", asyncHandler(async (req, res) => {
+  const tweets = await Tweet.findAll();
+  res.json({ tweets })
+}));
+
+router.get('/:id(\\d+)', asyncHandler(async (req, res, next) => {
+  const tweetId = parseInt(req.params.id);
+  const tweet = await Tweet.findByPk(tweetId);
+
+  if (tweet) {
+    res.json({ tweet })
+  }
+  else {
+    next(tweetNotFoundError)
+  }
+}));
+
+
+
+module.exports = router;
