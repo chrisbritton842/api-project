811c3b18050580d340dd91f2108fce9a3960aa16
daniel <danielthai14@gmail.com>
2022-01-13 11:20:46 -0700

    add event listener

--- express-apis-frontend/public/js/index.js
+++ express-apis-frontend/public/js/index.js
@@ -1,1 +1,17 @@
-console.log("Hello from index.js!");
+
+document.addEventListener("DOMContentLoaded", async () => {
+    try {
+      const res = await fetch("http://localhost:8080/tweets");
+      const { tweets } = await res.json();
+      console.log(tweets);
+
+    if (res.status === 401) {
+        res.redirect('/log-in')
+        return
+        } else {
+            
+        }
+    } catch (e) {
+      console.error(e);
+    }
+  });
